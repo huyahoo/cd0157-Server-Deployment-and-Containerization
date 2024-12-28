b253075b4ece46aa30aeee094500ab452bcfb646
HUY <takasugi.tut@gmail.com>
2024-12-28 20:21:16 +0900

    modify buildspec.yml

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ import jwt
 from flask import Flask, jsonify, request, abort
 
 
-JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
-LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
+JWT_SECRET = os.environ.get('JWT_SECRET')
+LOG_LEVEL = os.environ.get('LOG_LEVEL')
 
 
 def _logger():
     '''
     Setup logger format, level, and handler.
+
     RETURNS: log object
     '''
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -37,20 +38,19 @@ LOG = _logger()
 LOG.debug("Starting with log level: %s" % LOG_LEVEL )
 APP = Flask(__name__)
 
-
 def require_jwt(function):
     """
     Decorator to check valid jwt is present.
     """
     @functools.wraps(function)
     def decorated_function(*args, **kws):
-        if 'Authorization' not in request.headers:
+        if not 'Authorization' in request.headers:
             abort(401)
         data = request.headers['Authorization']
-        token = data.replace('Bearer ', '')
+        token = str.replace(str(data), 'Bearer ', '')
         try:
             jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
-        except jwt.InvalidTokenError:
+        except: # pylint: disable=bare-except
             abort(401)
 
         return function(*args, **kws)
@@ -72,45 +72,44 @@ def auth():
     password = request_data.get('password')
     if not email:
         LOG.error("No email provided")
-        return jsonify({"message": "Missing parameter: email"}), 400
+        return jsonify({"message": "Missing parameter: email"}, 400)
     if not password:
         LOG.error("No password provided")
-        return jsonify({"message": "Missing parameter: password"}), 400
+        return jsonify({"message": "Missing parameter: password"}, 400)
+    body = {'email': email, 'password': password}
 
-    user_data = {'email': email}
-    return jsonify(token=_get_jwt(user_data))
+    user_data = body
+
+    return jsonify(token=_get_jwt(user_data).decode('utf-8'))
 
 
 @APP.route('/contents', methods=['GET'])
-@require_jwt
 def decode_jwt():
     """
     Check user token and return non-secret data
     """
+    if not 'Authorization' in request.headers:
+        abort(401)
     data = request.headers['Authorization']
-    token = data.replace('Bearer ', '')
+    token = str.replace(str(data), 'Bearer ', '')
     try:
-        payload = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
-    except jwt.InvalidTokenError:
+        data = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
+    except: # pylint: disable=bare-except
         abort(401)
 
-    response = {
-        'email': payload['email'],
-        'exp': payload['exp'],
-        'nbf': payload['nbf'],
-    }
-    return jsonify(response)
+
+    response = {'email': data['email'],
+                'exp': data['exp'],
+                'nbf': data['nbf'] }
+    return jsonify(**response)
 
 
 def _get_jwt(user_data):
     exp_time = datetime.datetime.utcnow() + datetime.timedelta(weeks=2)
-    payload = {
-        'exp': exp_time,
-        'nbf': datetime.datetime.utcnow(),
-        'email': user_data['email']
-    }
+    payload = {'exp': exp_time,
+               'nbf': datetime.datetime.utcnow(),
+               'email': user_data['email']}
     return jwt.encode(payload, JWT_SECRET, algorithm='HS256')
-
 
 if __name__ == '__main__':
     APP.run(host='127.0.0.1', port=8080, debug=True)
